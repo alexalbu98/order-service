16363c0fa57482c611df5b22b532fb56fb082576
Alex Albu <alexalbu78@gmail.com>
2024-11-14 14:01:21 +0200

    Created order controller.
    Signed-off-by: Alex Albu <alexalbu78@gmail.com>

---
+++ src/main/java/com/polarbookshop/order_service/web/OrderController.java
@@ -0,0 +1,30 @@
+package com.polarbookshop.order_service.web;
+
+import com.polarbookshop.order_service.domain.Order;
+import com.polarbookshop.order_service.domain.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(
+            @RequestBody @Valid OrderRequest orderRequest
+    ) {
+        return orderService.submitOrder(
+                orderRequest.isbn(), orderRequest.quantity()
+        );
+    }
+}
