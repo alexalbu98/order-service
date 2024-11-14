5fa6e9824bd0733cf367d7bdef63e359b2e32b01
Alex Albu <alexalbu78@gmail.com>
2024-11-14 14:25:11 +0200

    Updated order service to communicated with catalog service.
    Signed-off-by: Alex Albu <alexalbu78@gmail.com>

--- src/main/java/com/polarbookshop/order_service/domain/OrderService.java
+++ src/main/java/com/polarbookshop/order_service/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.order_service.domain;
 
+import com.polarbookshop.order_service.book.Book;
+import com.polarbookshop.order_service.book.BookClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -9,15 +11,24 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final BookClient bookClient;
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        return Mono
-                .just(buildRejectedOrder(isbn, quantity))
+        return bookClient.getBookByIsbn(isbn)
+                .map(book -> buildAcceptedOrder(book, quantity))
+                .defaultIfEmpty(
+                        buildRejectedOrder(isbn, quantity)
+                )
                 .flatMap(orderRepository::save);
+    }
+
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.of(book.isbn(), book.title() + " - " + book.author(),
+                book.price(), quantity, OrderStatus.ACCEPTED);
     }
 
     public static Order buildRejectedOrder(
